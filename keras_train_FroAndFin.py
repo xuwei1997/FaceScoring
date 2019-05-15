4b28a7f791c679e5ba0b82c3cc4553ae63d3b52e
xuwei <xuwei@xu-wei.cn>
2019-05-15 16:49:40 +0800

    20190515

--- keras_train_FroAndFin.py
+++ keras_train_FroAndFin.py
@@ -3,23 +3,24 @@
 
 from keras.models import Sequential
 from keras.layers import Dense
-from keras.optimizers import Adam
+from keras.optimizers import Adam,SGD
 import numpy as np
 from keras.applications import ResNet50
 import matplotlib.pyplot as plt
 
 # tf.test.gpu_device_name()
 model_name = "FrozenAndFinetuning"  # 模块命名，用于绘图时
+train_epochs0 = 2  # 设置冻结训练轮次
+train_epochs1 = 2  # 设置微调训练轮次
 
-
-def show_history_mse(history0):  # 绘制mse图像
-    plt.plot(history0['loss'])
-    plt.plot(history0['val_loss'])
-    plt.title(model_name + ' mse')
+def show_history_mse2(history0,history1):  # 绘制mse图像
+    plt.plot(history0.history['loss']+history1.history['loss'])
+    plt.plot(history0.history['val_loss']+history1.history['val_loss'])
+    plt.title(model_name+' mse')
     plt.ylabel('loss')
     plt.xlabel('Epoch')
     plt.legend(['Train', 'Test'], loc='upper left')
-    plt.savefig('drive/app/' + model_name + '_mse.jpg')
+    plt.savefig('drive/app/'+model_name+'_mse.jpg')
     plt.show()
 
 
@@ -52,7 +53,7 @@ print("compile")
 model.compile(loss='mean_squared_error', optimizer=Adam())
 
 print("fit")
-Hist = model.fit(x_train, y_train, epochs=1, batch_size=64, validation_data=(x_test, y_test))
+Hist = model.fit(x_train, y_train, epochs=2, batch_size=64, validation_data=(x_test, y_test))
 
 model.save_weights('drive/app/weight.h5')
 print(Hist.history)
@@ -71,16 +72,15 @@ for layer in model2.layers:
 print(model2.summary())
 
 print("compile")
-model2.compile(loss='mean_squared_error', optimizer=Adam())
+model2.compile(loss='mean_squared_error', optimizer=Adam(lr=0.0005))
 
 print("fit")
-Hist2 = model2.fit(x_train, y_train, epochs=1, batch_size=64, validation_data=(x_test, y_test))
+Hist2 = model2.fit(x_train, y_train, epochs=2, batch_size=64, validation_data=(x_test, y_test))
 print(Hist2.history)
 
 # 输出图像---------------------------------------------
+show_history_mse2(Hist,Hist2)
 
-show_history_mse(Hist.history)
-show_history_mse(Hist2.history)
 # show_history_ce(Hist)
 # loss_and_metrics = model.evaluate(x_test, y_test, batch_size=128)
 # print(loss_and_metrics)
